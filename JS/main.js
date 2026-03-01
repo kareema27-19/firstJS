60d89eca375b176920249945596780fa6967b74f
kareema27-19 <kareemaalsaadi1@gmail.com>
2026-03-01 11:38:44 +0400

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,708 @@
+// document.getElementById("btn").addEventListener("click",
+//     function(){
+//     alert("heloooo")
+// }
+// )
+// var x=45
+// console.log(x,typeof(x));
+// var y="Kareema";
+// console.log(y,typeof(y));
+// var z= true
+// console.log(z,typeof(z));
+
+
+
+// console.log(s);
+
+
+
+// console.log prompt(enter your name);
+
+// var fname= prompt("enter first name")
+// var lname= prompt("enter last name")
+// var age=prompt("enter your age")
+// console.log(`hello my firs name is ${fname} and my last name is ${lname} and my age is ${age}`);
+
+// var num1= prompt("enter number1")
+// var num2= prompt("enter number2")
+// console.log(sum);
+
+
+// var number=(prompt("enter any number"))
+
+
+// var num1= +prompt("enter num1")
+// var num2= +prompt("enter num2")
+// console.log(num1+num2);
+
+// var user = {
+//     name: "Kareema", id: "4947176", city: "suwq", child: {
+//         name: "Ahsha",
+//         gender: "femail",
+//         dob: "30-11-2009",
+//         hobbies: {
+//             sports:"football",
+//             subjects:"math",
+//             color:{
+
+//             }
+//         }
+//     }
+
+// }
+// console.log(user.child.hobbies.subjects);
+
+// var user1={
+//     id:"15247",
+//      name:"Yahia",
+//      child:{
+//         name:"ahmed",
+//         gender:"male"
+
+
+
+// }
+// }
+// console.log(user1["child"]["gender"]);
+
+// var friends =["fales","salem","14724","mona","true","87489","salah"]
+// console.log(friends[0]);
+// console.log(friends[1]);
+// console.log(friends[2]);
+// console.log(friends[3]);
+// console.log(friends[4]);
+// console.log(friends[3]);
+// console.log(friends[3]);
+
+// var friends=[
+//      {name:"yahiha"},
+//     {id:"214721"},
+//     {city:"sur"},
+//     {phone:"94217300"},
+
+// ]
+// console.log(friends[0]);
+// console.log(friends[1]);
+// console.log(friends[2]);
+// console.log(friends[3]);
+// var BigArray = [
+// [[1,2,3],[1,2,3]],
+// [[1,2,3],[1,2,3],["Talaal","Yousef"]],
+// [["Ahemd","Eid","Saieed"],[1,2,3]],
+// [["Mahmoud","Hassan","Soliman"],[1,2,3]],
+// [[1,2,3],[1,2,3],
+
+// [{name:"Monsef",id:[45,45,45,["SARA"]]}]],
+// ]
+
+
+
+// console.log(BigArray[4][2][0].name);
+
+//4
+//2
+//0
+// var num=81
+// if(num>=0)
+//     console.log("positive");
+// else console.log("negative");
+
+
+
+// sum=0;
+// average=0;
+// for(var i=1; i<=5; i+=1){
+//     var mark = Number( prompt ("Enter mark"+1));
+
+//         console.log("student " +i +"mark "+mark);
+
+//          if(mark>=50)
+
+//              console.log("student " +i +"mark "+ "pass");
+//     else
+//         console.log("student " +i +"mark "+ "fail");}
+// sum=sum+mark;
+// average=sum/5;
+// console.log("total" +sum);
+// console.log("average" +average);
+
+
+// for(var i=1 ; i<=100; i+=1){
+
+//     if(i%3==0 && i%5==0){
+//         console.log("fizzbuzz")  
+//     }
+//     else if(i%3==0){
+//          console.log("fizz")}
+//     else if(i%5==0){
+//         console.log("buzz")}
+//         else
+//             console.log(i);
+
+
+
+// }
+// for(var i=1 ; i<=100 ; i++){
+//     console.log(i);
+
+// }
+// for(  ;   ;  ){
+//     var x= Number (prompt("enter number"))
+// if ( x==10 )
+//     break
+//     else{
+//         console.log(x);
+
+//      continue
+// }
+// }
+
+// for(  ;   ;  ){
+//     var x= Number (prompt("enter number"))
+//      if ( x==10 )
+//    break
+//      else{
+//          console.log(x);
+
+//    continue
+//  }
+//  }
+
+// for(  ;   ;  ){
+//          var x= Number (prompt("enter number"))
+//       if ( x==10 )
+//     break
+//      else{
+//           console.log(x);
+
+//     continue
+//  }
+//   }
+
+// var random = Math.floor(Math.random() * 10);
+// for(  ;   ;  ){
+//     var number= number (prompt("enter number"))
+// if(number==random)break
+// console.log("randomnumber");
+
+// }
+
+// var random = Math.floor(Math.random() * 10)
+// console.log(random)
+// for (var i = 0; i < 5; i++) {
+//     var x = +prompt("enter number")
+//     if (x > random) {
+//         console.log("too hot")
+//         continue
+//     }
+//     else if (x < random) {
+//         console.log("too cold")
+//         continue
+//     }
+//     else {
+//         console.log("Well done ");
+//         break
+
+//     }
+
+
+
+
+// var userEmail = prompt("enter your email")
+
+// var userpass = prompt("enter your password")
+
+// Hilal = 0
+//  attempts = 0;
+
+// while (userEmail != "ahmed@gmail" || userpass != "12345A") {
+//     Hilal++
+//     if (Hilal <= 3) {
+//         userEmail = prompt("enter your email")
+
+//         userpass = prompt("enter your password")
+
+
+//     }
+//     else break
+
+
+//  var userEmail = prompt("enter email")
+
+//  var userpass = prompt("enter password")
+// var number_of_tries =0
+// while(userEmail !="aaaa" || userpass !="45"){
+//     number_of_tries++
+//     if(number_of_tries < 3){
+//         userEmail = prompt("enter email")
+//         userEmail = prompt("enter password")
+//     }
+//     else{
+//         alert("you tried too many times")
+//         break
+//     }
+// }
+// if(userEmail=="aaaa" && userpass=="45"){
+
+//     alert("successfully logged in")
+
+
+//     var number_of_questions = +prompt("please enter your number of questions")
+//     var operators =["+","-","*","/","**","%"]
+//     var score=0
+// }
+// for(var i= 1 ; i <= number_of_questions ; i++){
+
+//     var num1 = Math.floor(Math.random() * 10)
+
+//     var num2 = Math.floor(Math.random() * 10)
+
+//     var operation = operators[ Math.floor(Math.random() * operators.length)]
+
+
+//     var useranswer= +prompt(`${ i } ${ num1 } ${ operation } ${ num2 } =`);}
+
+
+//     var correctanswer;
+
+// if (operation === "+") {
+// correctAnswer = num1 + num2;
+// } else if (operation === "-") {
+// correctanswer = num1 - num2;
+// } else if (operation === "*") {
+// correctanswer = num1 * num2;
+// } else if (operation === "**") {
+// correctanswer = num1 ** num2;
+// } else if (operation === "/") {
+
+// correctanswer = num1 / num2;
+// } else if (operation === "%") {
+// correctanswer = num1 % num2;}
+
+
+
+// if (useranswer == correctanswer) {
+// score++;
+
+// }
+// {
+
+// alert("Your total score is: " + score + " out of " + number_of_questions);
+
+//  }  
+
+// function calcSum (x,y) {
+
+// var final_sum = x + y
+// return final_sum
+
+// }
+
+// console.log(calcSum(5,6));
+
+
+// var num1= + prompt("enter num1")
+// var num2= + prompt("enter num2")
+// var operation= prompt("enter operation(+,-,*,/,)")
+
+//  function operator (num1,num2,operation){
+
+//     if (operation === "+"){
+
+//         return num1 + num2;
+//     }
+//     else if
+//     (operation === "-"){
+
+//         return num1 - num2;
+//     }
+//     else if
+//     (operation === "*"){
+
+//         return num1 * num2;
+//     }
+//     else if
+//     (operation === "/"){
+
+//         return num1 / num2;
+//     }
+
+// }
+//         console.log(operator (num1,num2,operation));
+
+
+// function sayHI (num1,num2){
+//     console.log("num1=", num1 , " num2=" ,num2 );
+
+// }
+// sayHI(undefined,15)
+
+// (function(){
+//     var x= 50
+//     console.log(x);
+
+
+// })();
+
+
+// (function(){
+//     var x= 41
+//     console.log(x);
+
+// })()
+
+// let num1 = + prompt("enter num1")
+// let num2 = + prompt("enter num2")
+// let num3 = + prompt("enter num3")
+
+
+// let sum =(num1,num2,num3) => (num1 + num2 + num3)/3
+
+// console.log(sum(num1,num2,num3));
+
+// var word = prompt ("enter your name")
+// document.getElementById("main").innerText = "hello"+ word
+
+
+// let btn = document.querySelector("#btn")
+// btn.addEventListener("mouse enter");
+// console.log("mouse entered");
+// btn.addEventListener("mouse leave");
+// console.log("mouse leaved");
+
+
+// btn.addEventListener("click", function(){
+
+//     if (btn.style.backgroundColor === "red") {
+//      btn.style.backgroundColor = "yellow";
+// }
+
+// else {
+//     btn.style.backgroundColor = "red";
+// }
+// // });
+// document.addEventListener("click",function(e){
+//     console.log(e.target);
+
+// })
+
+
+
+// let email= document.querySelector("#Fname")
+// let pass= document.querySelector("#Lname")
+// let login= document.querySelector("#btn")
+
+
+// login.addEventListener("click",(e)=>{
+
+// console.log(email.value,pass.value);
+// })
+
+
+
+
+
+
+// let email = document.querySelector("#Fname");
+// let pass = document.querySelector("#Lname");
+// let logbtn = document.querySelector("#btn");
+
+// let number_of_tries = 0;
+
+// logbtn.addEventListener("click", function () {
+
+//     if (email.value == "aaaa" && pass.value == "45") {
+
+//         alert("successfully logged in");
+
+//         let number_of_questions = +document.getElementById("number of Questions").value;
+//         let operations = ["+", "-", "*", "/"];
+//         let score = 0;
+//         let currentQuestion = 0;
+
+//         for (let i = 1; i <= number_of_questions; i++) {
+
+//             let num1 = Math.floor(Math.random() * 10);
+//             let num2 = Math.floor(Math.random() * 10) + 1;
+
+//             let operation = operations[Math.floor(Math.random() * operations.length)];
+
+//             let correctAnswer;
+
+//             if (operation == "+") {
+//                 correctAnswer = num1 + num2;
+//             }
+//             else if (operation == "-") {
+//                 correctAnswer = num1 - num2;
+//             }
+//             else if (operation == "*") {
+//                 correctAnswer = num1 * num2;
+//             }
+//             else {
+//                 correctAnswer = num1 / num2;
+//                 document.getElementById("Questions").innerHTML =
+//                     `Questions ${currentQuestion + 1}: ${num1} ${operation} ${num2} = ?`;
+
+//                 document.getElementById("userAnswer").value = "";
+//             }
+//         }
+
+//         // let userAnswer = +prompt(num1 + " " + operation + " " + num2 + " = ?");
+
+//         function checkAnswer() {
+//              let userAnswer = document.getElementById("userAnswer").value;
+//         if (userAnswer == correctAnswer) {
+//             alert("Correct ");
+//             score++;
+//         }
+//         else {
+//             alert("Wrong correct answer is: " + correctAnswer);
+//         }
+//     }
+
+//     alert("Your score is: " + score + " out of " + number_of_questions);
+// }
+//     else {
+
+//         number_of_tries++;
+
+// if (number_of_tries < 3) {
+//     alert("Wrong email or password");
+// }
+// else {
+//     alert("you tried many times");
+
+// }
+//     }
+
+// });
+
+
+// let email = document.querySelector("#Fname");
+// let pass = document.querySelector("#Lname");
+// let logbtn = document.querySelector("#btn");
+
+// let number_of_tries = 0;
+
+// let number_of_questions;
+// let operations = ["+", "-", "*", "/"];
+// let score = 0;
+// let currentQuestion = 0;
+// let correctAnswer;
+
+// logbtn.addEventListener("click", function () {
+
+//     if (email.value == "aaaa" && pass.value == "45") {
+
+//         alert("successfully logged in");
+
+//         number_of_questions = +document.getElementById("number_of_Questions").value;
+//         score = 0;
+//         currentQuestion = 0;
+
+//         nextQuestion();
+//     }
+//     else {
+
+//         number_of_tries++;
+
+//         if (number_of_tries < 3) {
+//             alert("Wrong email or password");
+//         }
+//         else {
+//             alert("you tried many times");
+//         }
+//     }
+// });
+
+// function nextQuestion() {
+
+//     if (currentQuestion <= number_of_Questions) {
+//         alert("Your score is: " + score + " out of " + number_of_Questions);
+//         return;
+//     }
+
+//     let num1 = Math.floor(Math.random() * 10) + 1;
+//     let num2 = Math.floor(Math.random() * 10) + 1;
+
+//     let operation = operations[Math.floor(Math.random() * operations.length)];
+
+//     if (operation == "+") {
+//         correctAnswer = num1 + num2;
+//     }
+//     else if (operation == "-") {
+//         correctAnswer = num1 - num2;
+//     }
+//     else if (operation == "*") {
+//         correctAnswer = num1 * num2;
+//     }
+//     else {
+//         correctAnswer = (num1 / num2).toFixed(2);
+//     }
+
+//     document.getElementById("Questions").innerHTML =
+//         `Question ${currentQuestion + 1}: ${num1} ${operation} ${num2} = ?`;
+// }
+
+// function checkAnswer() {
+
+//     let userAnswer = document.getElementById("userAnswer").value;
+
+//     if (userAnswer == correctAnswer) {
+//         alert("Correct");
+//         score++;
+//     }
+//     else {
+//         alert("Wrong, correct answer is: " + correctAnswer);
+//     }
+
+//     currentQuestion++;
+//     nextQuestion();
+// }
+// ===========================================================================================
+
+
+let userEmail = document.querySelector("#useremail");
+let userPass = document.querySelector("#userpass");
+let logbtn = document.querySelector("#logbtn");
+let errMess = document.querySelector("#errMess");
+let succMess = document.querySelector("#succMess");
+let form = document.querySelector("form");
+let QuestionsNum = document.querySelector("#questionsNum");
+let strTestBtn = document.querySelector("#strTestBtn");
+let testScreen = document.querySelector("#testScreen");
+let testSection = document.querySelector("#test");
+let logSection = document.querySelector("#logform");
+let cartona = ''
+let logoutBtn = document.querySelector("#logoutBtn");
+// local storage
+ //localStorage.setItem("isLoggedIn","true");
+
+
+
+
+if (localStorage.getItem("isLoggedIn") === "true") {
+    logSection.classList.add("d-none");
+    testSection.classList.remove("d-none");
+}
+
+logbtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    console.log(userEmail.value, userPass.value);
+
+
+
+    if (userEmail.value != "cc@" || userPass.value != "12345") {
+        localStorage.setItem("isLoggedIn", "true");
+        errMess.classList.replace("d-none", "d-block")
+    }
+    else {
+        errMess.classList.replace("d-block", "d-none")
+
+        succMess.classList.replace("d-none", "d-block")
+
+        localStorage.setItem("isLoggedIn", "true")
+        setTimeout(GoToTestPage,
+            1000
+        )
+    }
+
+
+})
+
+function GoToTestPage() {
+    testSection.classList.replace("d-none", "d-block")
+    form.classList.add("d-none")
+}
+
+strTestBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    let num = QuestionsNum.value
+    cartona = ""
+    let answers = []
+    for (let i = 1; i <= num; i++) {
+        let num1 = Math.floor(Math.random() * 10)
+        let num2 = Math.floor(Math.random() * 10)
+        answers.push(num1 + num2)
+        let opertation = "+"
+        cartona += `${i}) <label> ${num1} + ${num2} = </label>
+<input type="number" class='form-control answer-input'>`
+    }
+    cartona += `<button id="submitBtn" class="btn btn-success mt-3">Submit</button>
+<h3 id="score" class="mt-3"></h3>`;
+    testScreen.innerHTML = cartona; // توليد الأسئلة + زر Submit
+
+    let submitBtn = document.querySelector("#submitBtn"); // الزر الجديد
+    submitBtn.addEventListener("click", function () {
+        let score = 0;
+        let inputs = document.querySelectorAll(".answer-input");
+
+        for (let i = 0; i < inputs.length; i++) {
+            if (Number(inputs[i].value) === answers[i]) {
+                score++;
+            }
+        }
+
+        document.querySelector("#score").innerText = "Your Score: " + score + "/" + answers.length;
+    });
+
+});
+logoutBtn.addEventListener("click", function () {
+    localStorage.removeItem("isLoggedIn");
+    testSection.classList.add("d-none");
+    logSection.classList.remove("d-none");
+});
+// =================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
